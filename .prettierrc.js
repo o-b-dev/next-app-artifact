48c5a30426f6b96619624cd245e2e1da2f50cfc4
JUNER <juneruse@foxmail.com>
2025-06-15 15:57:43 +0800

    更新 Prettier 配置，添加 @radix-ui/react-slot 依赖，优化按钮组件样式，新增输入组件

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,10 @@
 module.exports = {
-    endOfLine: 'lf',
-    tabWidth: 2,
-    useTabs: false,
-    semi: false,
-    printWidth: 120,
-    singleQuote: true,
-    trailingComma: 'none',
-    plugins: ['prettier-plugin-packagejson', 'prettier-plugin-tailwindcss'],
-  }
-  
-
-
-  
+  endOfLine: 'lf',
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'none',
+  plugins: ['prettier-plugin-packagejson', 'prettier-plugin-tailwindcss']
+}
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div className="flex min-h-svh items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="bg-red-500 text-2xl font-bold">Hello World</h1>
+        <h1 className="text-2xl font-bold">Hello World</h1>
         <Button size="sm">Button</Button>
       </div>
     </div>
